83bd28c57dfd9b9e031b1bf851ca1af58a031eca
hebcodee <herberth.ferreira6@gmail.com>
2026-02-23 20:02:09 -0300

    feat: implement base structure for tests

---
+++ domain/src/test/java/br/com/alura/marketplace/domain/usecase/CadastroProdutoUseCaseTest.java
@@ -0,0 +1,29 @@
+package br.com.alura.marketplace.domain.usecase;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CadastroProdutoUseCaseTest {
+
+    @DisplayName("Quando cadastrar produto")
+    @Nested
+    class Cadastrar{
+
+        @DisplayName("Entao deve executar com sucesso")
+        @Nested
+        class Sucesso{
+
+            @DisplayName("Dado um produto com todos os campos")
+            @Test
+            void teste1(){
+
+            }
+        }
+
+    }
+
+}
