fa25e5b40c8b3b082a258b1797f222b620e6bef6
hebcodee <herberth.ferreira6@gmail.com>
2026-03-04 23:57:03 -0300

    feat: test with Rest Assured

---
+++ iandt/src/test/java/br/com/alura/marketplace/iandt/CadastroProdutoTest.java
@@ -0,0 +1,67 @@
+package br.com.alura.marketplace.iandt;
+
+import br.com.alura.marketplace.application.Application;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static br.com.alura.marketplace.application.v1.dto.factory.ProdutoDtoFactory.criarProdutoDtoRequest;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ContextConfiguration(classes = Application.class)
+@Testcontainers
+public class CadastroProdutoTest {
+
+    @LocalServerPort
+    Integer port;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @DisplayName("Quando criar um produto")
+    @Nested
+    class CadastrarProduto {
+
+        @DisplayName("Então deve executar com sucesso")
+        @Nested
+        class Sucesso {
+            @DisplayName("Dado um produto com todos os campos")
+            @Test
+            void teste1() throws JsonProcessingException {
+                // Dado
+                var produto = criarProdutoDtoRequest()
+                        .comTodosOsCampos();
+
+                // Quando
+                var response = given()
+                        .log().all()
+                        .header("Correlation-Id", "ff81d6d4-cb83-4877-bdfd-c6a80522cf42")
+                        .contentType(JSON)
+                        .body(objectMapper.writeValueAsString(produto))
+                        .post("/v1/produtos")
+                        .then()
+                        .log().all()
+                        .extract()
+                        .response();
+
+                // Então
+                assertThat(response.statusCode())
+                        .isEqualTo(201);
+            }
+
+        }
+    }
+}
