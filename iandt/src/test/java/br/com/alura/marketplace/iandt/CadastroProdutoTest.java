7602580d62e7535f5c6a405bc6ce10af76988cc7
hebcodee <herberth.ferreira6@gmail.com>
2026-03-05 00:35:41 -0300

    feat: add setup for increase legibility tests

--- iandt/src/test/java/br/com/alura/marketplace/iandt/CadastroProdutoTest.java
+++ iandt/src/test/java/br/com/alura/marketplace/iandt/CadastroProdutoTest.java
@@ -1,51 +1,35 @@
 package br.com.alura.marketplace.iandt;
 
 import br.com.alura.marketplace.application.Application;
+import br.com.alura.marketplace.iandt.setup.LocalStackSetup;
+import br.com.alura.marketplace.iandt.setup.WiremockSetup;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.s3.S3Template;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import static br.com.alura.marketplace.application.v1.dto.factory.ProdutoDtoFactory.criarProdutoDtoRequest;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ContextConfiguration(classes = Application.class)
 @Testcontainers
-public class CadastroProdutoTest {
-
-    public final static LocalStackContainer LOCAL_STACK = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3.5.0"))
-            .withServices(S3);
-
-    @DynamicPropertySource
-    static void localstackDynamicPropertySource(DynamicPropertyRegistry registry) {
-        registry.add("spring.cloud.aws.region.static", LOCAL_STACK::getRegion);
-        registry.add("spring.cloud.aws.credentials.access-key", LOCAL_STACK::getAccessKey);
-        registry.add("spring.cloud.aws.credentials.secret-key", LOCAL_STACK::getSecretKey);
-        registry.add("spring.cloud.aws.s3.endpoint", () -> LOCAL_STACK.getEndpointOverride(S3));
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        LOCAL_STACK.start();
-    }
+public class CadastroProdutoTest implements LocalStackSetup, WiremockSetup {
 
     @LocalServerPort
     Integer port;
